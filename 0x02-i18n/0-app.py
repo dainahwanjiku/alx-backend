14e9e77de3bfa1db279c4518e31153292c5ff718
dainahwanjiku <deeshish2017@gmail.com>
2023-01-31 23:12:00 +0000

    basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,14 +2,15 @@
 """
 how to setup a basic Flask app
 """
-
-
 from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     """single route and an index.html template"""
     return render_template('0-index.html')
+
+
 if __name__ == '__main__':
     app.run()

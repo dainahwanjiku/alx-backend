03c0acb3531e96ac2a33d6c165277625356ad256
dainahwanjiku <deeshish2017@gmail.com>
2023-01-31 23:57:38 +0000

    basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/templates')
 def index():
     """single route and an index.html template"""
     return render_template('0-index.html')
